2949159dae4b21cd43c0eec791cd6fc9baa63d56
kolserdav <serega12101983@gmail.com>
2023-04-12 01:40:44 +0700

    initial

---
+++ server/src/main.rs
@@ -0,0 +1,54 @@
+// libretranslate --host 192.168.0.3  --api-keys --req-limit 100 --require-api-key-secret --disable-web-ui --threads 4
+
+#[macro_use]
+extern crate log;
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    env_logger::builder().format_timestamp(None).init();
+
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    debug!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
